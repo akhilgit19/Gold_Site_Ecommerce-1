fb9cd80b80582b00d0b63c441eaab08121303671
akhilgit19 <138980055+akhilgit19@users.noreply.github.com>
2024-08-16 19:38:43 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "VIZAG CITY",
+    title: "Vizag City",
     desc: "This is now in new commit",
     bg: "green",
   },
