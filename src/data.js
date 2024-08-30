ef3cd15504d239ee06d38e4e72906658a1d06f09
akhilgit19 <138980055+akhilgit19@users.noreply.github.com>
2024-08-30 10:04:44 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "Automated React-Nginx server app deployment using Jenkins done1
+    title: "Automated React-Nginx server app deployment using Jenkins done
       ",
     desc: "This is now in new commit",
     bg: "green",
