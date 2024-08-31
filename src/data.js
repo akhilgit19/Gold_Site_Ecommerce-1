70f82a683c26b0d78f128adedd5a80f4d2479e7f
akhilgit19 <138980055+akhilgit19@users.noreply.github.com>
2024-08-31 13:08:00 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "Iron Man",
+    title: "Sucessfully Completed automation using jenkins CI-CD",
     desc: "This is now in new commit",
     bg: "green",
   },
