b5fc982d5f3bc80b4b086ed846c7eed9254cf4c8
akhilgit19 <138980055+akhilgit19@users.noreply.github.com>
2024-08-31 07:26:34 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "Avengers",
+    title: "Avengers-End Game",
     desc: "This is now in new commit",
     bg: "green",
   },
