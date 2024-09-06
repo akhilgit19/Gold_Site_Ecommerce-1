de8eca2a01c3db6d4e0e360e2a5e492c8aeeb0d4
akhilgit19 <138980055+akhilgit19@users.noreply.github.com>
2024-09-06 23:22:28 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "CI-CD -55X",
+    title: "CI-CD -GX",
     desc: "This is now in new commit",
     bg: "green",
   },
