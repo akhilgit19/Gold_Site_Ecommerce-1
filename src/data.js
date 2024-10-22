e5cc1a4c99c7d7e3a69de3119dc061df7cde0394
akhilgit19 <138980055+akhilgit19@users.noreply.github.com>
2024-10-22 10:16:58 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "Venom",
+    title: "Venom-1",
     desc: "This is now in new commit",
     bg: "green",
   },
