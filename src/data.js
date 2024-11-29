f82f31f6b4d2a0196033b91428ef0b07fc262332
akhilgit19 <138980055+akhilgit19@users.noreply.github.com>
2024-11-29 22:33:03 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,8 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "xmen",
+    title: "xmen-1
+      ",
     desc: "This is now in new commit",
     bg: "green",
   },
