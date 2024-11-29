fc5f04ce69d431df93c1218b96fa6fb1919d6156
akhilgit19 <138980055+akhilgit19@users.noreply.github.com>
2024-11-29 07:30:13 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,8 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "doomday-Avengers",
+    title: "doomday-1-
+      Avengers",
     desc: "This is now in new commit",
     bg: "green",
   },
